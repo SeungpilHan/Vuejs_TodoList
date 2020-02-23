5867a3d3cdcf2f710485988fd972beb80c37e092
SeungpilHan <gkstmdvlf2@gmail.com>
2020-02-23 18:14:40 +0900

    complete

---
+++ src/components/Example.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">Panel heading without title</div>
+    <div class="panel-body">
+      Panel content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoPage',
+  data () {
+    return {
+      todos: [
+        {
+          name:'청소'
+        },
+        {
+          name:'블로그 쓰기'
+        },
+        {
+          name:'밥먹기'
+        },
+        {
+          name:'안녕'
+        }
+      ]
+    }
+  }
+}
+</script>
